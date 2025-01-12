53413c89a15955c264344c7fb6ed7b50f4d9b61a
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-12 23:39:06 +0530

    feat: create public subnet :adhesive_bandage:

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,3 +6,14 @@ resource "aws_vpc" "main_vpc" {
     Name = var.vpc_name
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public subnet ${count.index + 1}"
+  }
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -7,3 +7,18 @@ variable "cidr_block" {
   type        = string
   description = "CIDR ADDRESS for vpc"
 }
+
+variable "availability_zones" {
+  type        = string
+  description = "availability_zones list"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "cidr values for public subnet"
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "cidr value for private subnet"
+}
