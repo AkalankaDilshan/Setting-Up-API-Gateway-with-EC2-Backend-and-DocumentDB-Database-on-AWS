4243e11abd29162d3da7aa7c8535f4db7f4f3d6d
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-14 19:54:53 +0530

    feat: create private and public subnet resources

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -28,9 +28,63 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+# VPC Route Table Section - public route
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main_vpc.id
   tags = {
     Name = "${var.vpc_name}-igw"
   }
 }
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "${var.vpc_name}-public-route-table"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_rt_association" {
+  count          = length(aws_subnet.public_subnet)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# VPC Route Table Section - private route
+resource "aws_route_table" "private_rt" {
+  count  = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "${var.vpc_name}-private-route-table-${count.index}"
+  }
+}
+
+resource "aws_eip" "elastic_IP_address" {
+  count  = length(var.private_subnet_cidrs)
+  domain = "vpc"
+  tags = {
+    Name = "${var.vpc_name}-vpc-nat-gateway-eip-${count.index}"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  count         = length(var.private_subnet_cidrs)
+  allocation_id = aws_eip.elastic_IP_address[count.index].id
+  subnet_id     = element(aws_subnet.public_subnet[*].id, count.index)
+  tags = {
+    Name = "${var.vpc_name}-nat-gateway-${count.index}"
+  }
+}
+
+resource "aws_route" "private_route" {
+  count                  = length(var.private_subnet_cidrs)
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gateway[count.index].id
+}
+
