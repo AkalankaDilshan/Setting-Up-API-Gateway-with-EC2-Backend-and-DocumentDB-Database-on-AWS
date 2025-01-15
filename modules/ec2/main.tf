31b41f26900948c33587973b7ebb0bf993ab17de
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-15 10:17:22 +0530

    feat: roughly complete ec2 module; closes ec2 #3

---
+++ modules/ec2/main.tf
@@ -0,0 +1,34 @@
+data "aws_ami" "latest_ami" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+resource "aws_instance" "server_instance" {
+  ami                     = data.aws_ami.latest_ami.id
+  instance_type           = var.instance_type
+  subnet_id               = var.subnet_id
+  vpc_security_group_ids  = [var.vpc_security_group_id]
+  disable_api_termination = false
+  ebs_optimized           = false
+  root_block_device {
+    volume_type = var.ebs_volume_type
+    volume_size = var.ebs_volume_size
+    encrypted   = true
+  }
+
+  key_name = var.key_pair_name
+
+  tags = {
+    Name = var.instance_name
+  }
+}
