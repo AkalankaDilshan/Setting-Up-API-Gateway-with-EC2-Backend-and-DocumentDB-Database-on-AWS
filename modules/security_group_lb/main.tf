94174ae9d70988b4ccbe8449f68236259c4af084
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-17 07:16:16 +0530

    feat: complete lb sg ⛄

--- modules/security_group_lb/main.tf
+++ modules/security_group_lb/main.tf
@@ -2,7 +2,22 @@ resource "aws_security_group" "load_balancer_security_group" {
   name        = var.sg_name
   description = "allow HTTP request from any source"
   vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = var.sg_name
   }
 }
+
