071d4b536f480c6c8a2e5a91eecf746100939128
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-17 09:22:57 +0530

    chore: enable auto workflow

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -14,13 +14,14 @@ data "aws_ami" "latest_ami" {
 }
 
 resource "aws_instance" "server_instance" {
-  ami                     = data.aws_ami.latest_ami.id
-  instance_type           = var.instance_type
-  subnet_id               = var.subnet_id
-  vpc_security_group_ids  = [var.vpc_security_group_id]
-  user_data               = templatefile("${path.module}/init-script.sh", {})
-  disable_api_termination = false
-  ebs_optimized           = false
+  ami                         = data.aws_ami.latest_ami.id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.vpc_security_group_id]
+  user_data                   = templatefile("${path.module}/init-script.sh", {})
+  associate_public_ip_address = false
+  disable_api_termination     = false
+  ebs_optimized               = false
   root_block_device {
     volume_type = var.ebs_volume_type
     volume_size = var.ebs_volume_size
