1ddd6ce44116a71ba6e1c68cc002a488e9abf144
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-18 22:25:25 +0530

    feat: roulhy complete ALB; closes #4

--- modules/application-load-balancer/main.tf
+++ modules/application-load-balancer/main.tf
@@ -9,5 +9,43 @@ resource "aws_lb" "application_load_balancer" {
   tags = {
     Name = var.alb_name
   }
+}
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name        = var.alb_name
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = var.health_check_path
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    protocol            = "HTTP"
+  }
+
+  tags = {
+    Name = "${var.alb_name}-tg"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "alb_tg_attachment" {
+  for_each         = { for idx, id in var.var.targets_ids : idx => id }
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = each.value
+  port             = 80
 }
