8c9524a2fef3c25775550ddfa5e993e77f45a80f
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-18 13:24:09 +0530

    chore: remove dublicate az cidr

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "main_vpc" {
   source               = "./modules/vpc"
   vpc_name             = "Server-VPC"
   cidr_block           = "10.10.0.0/16"
-  availability_zones   = ["eu-north-1a", "eu-north-1a"]
+  availability_zones   = ["eu-north-1a", "eu-north-1b"]
   public_subnet_cidrs  = ["10.10.1.0/24", "10.10.2.0/24"]
   private_subnet_cidrs = ["10.10.3.0/24", "10.10.4.0/24"]
 }
