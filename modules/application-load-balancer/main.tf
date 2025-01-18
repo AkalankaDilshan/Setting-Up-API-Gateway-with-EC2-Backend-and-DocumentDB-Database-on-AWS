7c10649de2737106af84344867da8e92cde62122
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-18 22:36:32 +0530

    fix : syntax var error

--- modules/application-load-balancer/main.tf
+++ modules/application-load-balancer/main.tf
@@ -44,7 +44,7 @@ resource "aws_lb_target_group" "tg" {
 }
 
 resource "aws_lb_target_group_attachment" "alb_tg_attachment" {
-  for_each         = { for idx, id in var.var.targets_ids : idx => id }
+  for_each         = { for idx, id in var.targets_ids : idx => id }
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = each.value
   port             = 80
