643c5af5f76e375208788eafef0c7a8a48cee13f
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-23 23:43:36 +0530

    perf : call editable modules

--- main.tf
+++ main.tf
@@ -11,56 +11,47 @@ module "main_vpc" {
   private_subnet_cidrs = ["10.10.3.0/24", "10.10.4.0/24"]
 }
 
-# module "lb_security_group" {
-#   source  = "./modules/security_group_lb"
-#   sg_name = "lb-sg"
-#   vpc_id  = module.main_vpc.vpc_id
-# }
-# module "ec2_security_group" {
-#   source                          = "./modules/security_group_ec2"
-#   sg_name                         = "server-sg"
-#   vpc_id                          = module.main_vpc.vpc_id
-#   nat_gateway_eip                 = flatten([module.main_vpc.nat_gateway_eip])
-#   load_balancer_security_group_id = module.lb_security_group.lb_sg_id
-#   depends_on                      = [module.lb_security_group]
-# }
+module "lb_security_group" {
+  source  = "./modules/security_group_lb"
+  sg_name = "lb-sg"
+  vpc_id  = module.main_vpc.vpc_id
+}
+module "ec2_security_group" {
+  source                          = "./modules/security_group_ec2"
+  sg_name                         = "server-sg"
+  vpc_id                          = module.main_vpc.vpc_id
+  nat_gateway_eip                 = flatten([module.main_vpc.nat_gateway_eip])
+  load_balancer_security_group_id = module.lb_security_group.lb_sg_id
+  depends_on                      = [module.lb_security_group]
+}
 
-# module "key_pair" {
-#   source = "./modules/key-pair-ssh"
-# }
+module "key_pair" {
+  source = "./modules/key-pair-ssh"
+}
 
-# module "server_instance_1" {
-#   source                = "./modules/ec2"
-#   instance_name         = "server-instance-1"
-#   instance_type         = "t3.micro"
-#   subnet_id             = module.main_vpc.private_subnet_id[0]
-#   vpc_security_group_id = module.ec2_security_group.ec2_sg_id
-#   ebs_volume_size       = 8
-#   ebs_volume_type       = "gp2"
-#   # key_pair_name         = module.key_pair.key_pair_name
-#   depends_on = [module.ec2_security_group, module.key_pair]
-# }
+module "private_ec2" {
+  source                = "./modules/ec2"
+  instance_name         = "private-ec2"
+  instance_type         = "t3.micro"
+  count                 = length(module.main_vpc.private_subnet_id)
+  subnet_id             = module.main_vpc.private_subnet_id[count.index]
+  vpc_security_group_id = module.ec2_security_group.ec2_sg_id
+  ebs_volume_size       = 8
+  ebs_volume_type       = "gp2"
+  key_pair_name         = module.key_pair.key_pair_name
+  depends_on            = [module.ec2_security_group, module.key_pair]
+}
 
-# module "server_instance_2" {
-#   source                = "./modules/ec2"
-#   instance_name         = "server-instance-2"
-#   instance_type         = "t3.micro"
-#   subnet_id             = module.main_vpc.private_subnet_id[1]
-#   vpc_security_group_id = module.ec2_security_group.ec2_sg_id
-#   ebs_volume_size       = 8
-#   ebs_volume_type       = "gp2"
-#   # key_pair_name         = module.key_pair.key_pair_name
-#   depends_on = [module.ec2_security_group, module.key_pair]
-# }
 
-# module "load_balancer" {
-#   source             = "./modules/application-load-balancer"
-#   alb_name           = "app-lb"
-#   load_balancer_type = "application"
-#   vpc_id             = module.main_vpc.vpc_id
-#   alb_subnet_ids     = flatten(module.main_vpc.public_subnet_id)
-#   target_group_type  = "instance"
-#   targets_ids        = [module.server_instance_1.instance_private_ip, module.server_instance_2.instance_private_ip]
-#   security_group_id  = module.lb_security_group.lb_sg_id
-#   depends_on         = [module.lb_security_group, module.server_instance_1, module.server_instance_2]
-# }
+module "load_balancer" {
+  source             = "./modules/application-load-balancer"
+  count              = length(module.private_ec2.instance_id)
+  alb_name           = "app-lb"
+  load_balancer_type = "application"
+  vpc_id             = module.main_vpc.vpc_id
+  alb_subnet_ids     = flatten(module.main_vpc.public_subnet_id)
+  target_group_type  = "instance"
+  target_id          = module.private_ec2.instance_id[count.index]
+  security_group_id  = module.lb_security_group.lb_sg_id
+  depends_on         = [module.lb_security_group, module.private_ec2]
+}
