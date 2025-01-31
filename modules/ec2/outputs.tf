9599f7ec04ca92874b38e4fa7b3fad9c74d2dd7c
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-31 10:07:30 +0530

    fix : ec2 ip output issue

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,7 +1,7 @@
 output "instance_id" {
-  value = aws_instance.server_instance[*].id
+  value = aws_instance.server_instance.id
 }
 
 output "instance_private_ip" {
-  value = aws_instance.server_instance[*].private_ip
+  value = aws_instance.server_instance.private_ip
 }
